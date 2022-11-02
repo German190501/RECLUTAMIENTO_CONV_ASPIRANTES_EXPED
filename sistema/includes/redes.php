f902fe0c23e739875568086002308c1b8df490ae
Francis Flores Salas <87655400+Francis-Darkness@users.noreply.github.com>
2022-11-01 20:16:41 -0600

    Delete sistema directory

--- sistema/includes/redes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-                        <div class="col-md-3">
-                            <a class="bt-facebook btn btn-primary w-100" role="button" href="https://www.facebook.com/sharer/sharer.php?u=https://utnequipo0301.utn.red/reclutamiento_proyectoV1/converted_files/<?php echo $output; ?>">Facebook</a>
-                        </div>
-                      
